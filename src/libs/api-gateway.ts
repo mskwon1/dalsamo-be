afc99935ea7a20928736f2d17ec69822d20bd085
mskwon1 <mageeeeek@gmail.com>
2023-03-31 21:46:32 +0900

    Adds cors headers

--- src/libs/api-gateway.ts
+++ src/libs/api-gateway.ts
@@ -16,6 +16,10 @@ export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<
 export const formatJSONResponse = (response: Record<string, unknown>) => {
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
     body: JSON.stringify(response),
   };
 };
