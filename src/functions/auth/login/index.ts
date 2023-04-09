f0b5145db6461f97ab9c4b7afc8bfd8daaecb328
mskwon1 <mageeeeek@gmail.com>
2023-04-09 13:05:39 +0900

    BF

--- src/functions/auth/login/index.ts
+++ src/functions/auth/login/index.ts
@@ -15,7 +15,7 @@ const login: LambdaFunctionEntry = {
           },
         },
         cors: {
-          origins: [process.env.DALSAMO_WEB_ORIGIN],
+          origin: 'http://localhost:3000',
           methods: ['POST', 'OPTIONS'],
           allowCredentials: true,
         },
